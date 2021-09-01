b5a5d7362e5b3bf5d724a9286c3b7ed0242f05dc
megaduo <63999380+megaduo@users.noreply.github.com>
2021-09-01 14:01:27 +0800

    Update linuxtv.mk

--- v4l-dvb/origin/linuxtv.mk
+++ v4l-dvb/origin/linuxtv.mk
@@ -2,11 +2,11 @@ PKG_NAME := v4l-dvb-linuxtv
 
 PKG_SOURCE_PROTO := git
 PKG_SOURCE_URL := git://linuxtv.org/media_build.git
-PKG_SOURCE_VERSION := 069192365e2cec8b47f6e6701fb2aa50f763c602
-PKG_SOURCE_DATE := 2021-03-09
+PKG_SOURCE_VERSION := 	7253675c65ed84dc294ef25e2af873e8092be48b
+PKG_SOURCE_DATE := 2021-08-17
 
 V4L_SOURCE_URL := git://linuxtv.org/media_tree.git
-V4L_SOURCE_VERSION := 1cb3ff130f4d97dec2d1bee968a87f5d6e24dd60
+V4L_SOURCE_VERSION := 	9c3a0f285248899dfa81585bc5d5bc9ebdb8fead
 V4L_REPO_NAME := linux-media-linuxtv
 V4L_SOURCE_FILE := $(V4L_REPO_NAME)-$(PKG_SOURCE_DATE)-$(shell printf '%.8s' $(V4L_SOURCE_VERSION)).tar.bz2
 
